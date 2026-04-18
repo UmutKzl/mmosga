774daeb4b4718c7c0b9913a67f9433c78a86f5f8
UmutKzl <umut.exe@icloud.com>
2026-04-18 10:14:21 +0300

    Update mmosga.sh
    Disable keyboard autocorrection
    Add a bunch of apps to install
    Install Neovim distros

--- mmosga.sh
+++ mmosga.sh
@@ -34,11 +34,19 @@ defaults write com.apple.finder QLEnableTextSelection -bool true
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 echo "Finder settings are finished."
 
+# Keyboard
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+echo "Keyboard settings are finished."
+
 # Trackpad
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 echo "Trackpad settings are finished."
 
-# Accent color
+# Accent color (purple)
 defaults write -g AppleAccentColor -int 5
 defaults write -g AppleColorPreferences -dict AccentColor -int 5
 defaults write -g AppleHighlightColor -string "0.968627 0.831373 1.000000 Purple"
@@ -52,18 +60,44 @@ echo "Launch services settings are finished."
 killall Dock
 killall SystemUIServer
 killall Finder
+killall TextEdit
 echo "Restarted services."
 
 clear
-# Install apps
-apps="spotify google-chrome ungoogled-chromium firefox \
-lazygit neovim visual-studio-code godot zed emacs helix tmux \
-zellij fzf ripgrep bat eza zoxide gh python \
-wezterm alacritty kitty ghostty raycast mac-mouse-fix \
-steam epic-games gog-galaxy heroic luanti supertuxkart"
+
+# Choose apps and install them
+apps="spotify google-chrome ungoogled-chromium firefox vivaldi \
+lazygit visual-studio-code zed neovim emacs helix godot antigravity \
+cursor windsurf tmux zellij fzf ripgrep bat eza zoxide gh python wezterm \
+alacritty kitty ghostty raycast mac-mouse-fix betterdisplay caffeine rectangle \
+steam epic-games gog-galaxy parallels crossover heroic luanti supertuxkart obs \
+lm-studio ollama claude claude-code opencode chatgpt chatgpt-atlas llama.cpp"
 brew install $(gum choose --no-limit $apps --header "Select apps to install")
 
 # Rosetta 2
 if gum confirm "Do you want to install Rosetta 2?" ; then
   softwareupdate --install-rosetta --agree-to-license
 fi
+
+# LazyVim
+if command -v nvim >/dev/null; then
+    if [ ! -d "$HOME/.config/nvim" ] && gum confirm "Do you want to install a Neovim config?"; then
+        distro=$(gum choose --header "Neovim distro" nvchad lazyvim)
+
+        case "$distro" in
+            nvchad)
+                git clone https://github.com/NvChad/starter ~/.config/nvim
+                rm -rf ~/.config/nvim/.git
+                echo "Don't forget to read wiki https://nvchad.com/docs/quickstart/install"
+                ;;
+            lazyvim)
+                git clone https://github.com/LazyVim/starter ~/.config/nvim
+                rm -rf ~/.config/nvim/.git
+                ;;
+        esac
+    fi
+else
+    echo "Skipping LazyVim section because you already have a Neovim config."
+fi
+
+echo "Everything is got finished. Good luck!"
